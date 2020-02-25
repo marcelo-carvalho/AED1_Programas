c0b8c5ad052e9735abd9a3892a6f4658f10a8d9e
Marcelo Carvalho <mdecarvalhopereira@nevada.unr.edu>
2020-02-25 17:36:59 -0300

    Added primeira atividade

---
+++ Aula03/Atividade1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    float b = 0;
+    float h = 0;
+    float area_triangulo = 0;
+
+    printf("Este programa calcula area de um triangulo, baseado dois dados de entrada:\n");
+    printf(" b = base e h = altura.\n");
+    printf("Entre o valor de b: ");
+    scanf("%f", &b);
+    
+    printf("Entre o valor de h: ");
+    scanf("%f", &h);
+    printf("\n");
+
+    //Calculo da área do triângulo
+    area_triangulo = (b * h)/2;
+
+    printf("A area do triangulo e igual a %f\n", area_triangulo);
+    
+    return 0;
+}
