15f1b3ce9d759b353c6234b119c7fe9b743948b2
Marcelo Carvalho <mdecarvalhopereira@nevada.unr.edu>
2020-02-26 13:51:04 -0300

    added Aula04/Atividade01

---
+++ Aula04/Atividade01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float a =0;
+    float b =0;
+
+    printf("Informe dois numeros a e b e te darei a soma dos dois:\n");
+    printf("Informe o valor de a: ");
+    scanf("%f", &a);
+    printf("Informe o valor de b: ");
+    scanf("%f", &b);
+    printf("A soma de a e b = %f", a+b);
+
+    return 0;
+}
